8cfc4354689fd9e59e80c6753b2a35a4df90414b
moizsiddiqui639-star <moizsiddiqui639@gmail.com>
2026-09-01 22:57:22 +0500

    Delete js directory

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-// Project URL (Usi Settings -> API page par top par likha hoga)
-const SUPABASE_URL = "https://supabase.com/dashboard/project/pnodbyrryqozysbnuaom"; 
-
-// Publishable Key (Jo screenshot se aapne copy ki hai)
-const SUPABASE_ANON_KEY = "sb_publishable_YaK-jySKrNStad6lPq9qpQ_KvDubsxM";
-
-// Connection setup
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
